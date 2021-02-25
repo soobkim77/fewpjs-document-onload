4706210ce1e760351b58e2e24ea04ff6e1721465
Soo-Beom <soobeomkim77@gmail.com>
2021-02-25 13:14:09 -0800

    commit tree bab612e7c2f407b776bf4f0979bb924f873a5fe6

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let newP = document.querySelector("#text");
+    newP.textContent = "This is really cool!"
+  });
